72d3109e839609b1ae2ba89d6fd48e1688c712cb
ratne <ratne@DESKTOP-VBSGNK8>
2019-12-09 18:58:55 +0530

    testing 1st

---
+++ src/test/java/maven_smcrm_tests/trialforconflict.java
@@ -0,0 +1,15 @@
+package maven_smcrm_tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class trialforconflict {
+	@Test
+	public void oneMethod()
+	{
+		Reporter.log("hello minoo",true);
+		Reporter.log("Hello Trial for conflict");
+		
+	}
+
+}
